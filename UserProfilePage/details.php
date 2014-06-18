de5317bd52f2073715c6b32b5fb4b62d59c7ea26
ElhamMirafzali <e.mirafzali@gmail.com>
2014-06-19 03:31:17 +0430

    details

--- UserProfilePage/details.php
+++ UserProfilePage/details.php
@@ -71,27 +71,29 @@ disconnect_from_database($dbc);
     <div class="detailsPop">
     	<div id="title">
         	<span id="det">مشـخـصــات    کـامــل</span>
+            <span id="back"> بازگشــت </span>
         </div>    <!-- end of title -->
+      
         <div id="left">
             <div id="pic"> <img src="<?php echo IMG_DIR.$picture; ?>"   /> </div>
-            <!--<div id="logo" <img src="-->
+            <?php if (!empty($facebook_profile)) { ?><div > <div id="fbBD"><a href="<?php echo $facebook_profile; ?>" target="_blank"><span id="fb"><?php echo $facebook_profile; ?></span></a> </div> <span>فیسبــوک <img src="fb.png" height="20em" width="20em" /></span> </div><?php } ?>
+            <?php if (!empty($google_profile)) { ?><div > <div id="googleBD"><a href="<?php echo $google_profile; ?>" target="_blank"><span id="google"><?php echo $google_profile; ?></span></a></div><span>گوگــل <img src="g.png"  height="20em" width="20em"  /> </span> </div><?php } ?>
+            <?php if (!empty($instagram_profile)) { ?><div > <div id="instaBD"><a href="<?php echo $instagram_profile; ?>" target="_blank"><span id="insta"><?php echo $instagram_profile; ?></span></a></div> <span>اینستاگـرام <img src="ig.png"  height="20em" width="20em"/> </span> </div><?php } ?>
+            <?php if (!empty($linkin_profile)) { ?><div > <div id="linkedinBD"><a href="<?php echo $linkin_profile; ?>" target="_blank"><span id="linkedin"><?php echo $linkin_profile; ?></span></a></div> <span>لینکـدین <img src="in.png"  height="20em" width="20em" /> </span> </div><?php } ?>
+          
         </div>     <!-- end of left -->
        
     	<div id="right">
             <?php if (!empty($first_name)) { ?><div ><div id="nameBD"> <span id="name"><?php echo $first_name; ?></span></div><span>نـام </span> </div><?php } ?>
             <?php if (!empty($last_name)) { ?><div ><div id="lastnameBD"><span id="lastname"><?php echo $last_name; ?></span></div><span >نـام خـانوادگی</span></div><?php } ?>
+            <div ><div id="birthBD"><span id="birth">25/3/1374</span></div><span>تـاریـخ تولـد </span></div>
+            <div ><div id="genderBD"><span id="gender">مــرد</span></div><span>جنســیت </span></div>
+            <div ><div id="degreeBD"><span id="degree">دیـپلم</span></div><span>میــزان تحصـیلات</span></div>
+            <div ><div id="zoneBD"><span id="zone">کامپیوتر</span></div><span>حــوزه فعـالیـت </span></div>
             <div ><div id="emailBD"><span id="email"><?php echo $email; ?></span></div><span>پست الکتـــرونیک </span></div>
             <?php if (!empty($phone_number)) { ?><div ><div id="phoneNumBD"><span id="phoneNum"><?php echo $phone_number; ?></span></div><span>شمـاره تلفـــن </span></div><?php } ?>
             <?php if (!empty($location)) { ?><div ><div id="locationBD"><span id="location"><?php echo $location; ?></span></div><span>شهـر / استـان </span> </div><?php } ?>
-            <br/>
-            <br/>
-          <!--  <div ><div id="degreeBD"><span id="degree">دلپــلم</span></div><span>مـدرک تـحصیـلی</span> </div>-->
-            <!--<div > <input type="text" size="30%"/> <span>حوزه فعـالیـت </span> </div>-->
-            <?php if (!empty($facebook_profile)) { ?><div > <div id="fbBD"><a href="<?php echo $facebook_profile; ?>" target="_blank"><span id="fb"><?php echo $facebook_profile; ?></span></a> </div> <span>فیسبــوک <img src="fb.png" height="20em" width="20em" /></span> </div><?php } ?>
-            <?php if (!empty($google_profile)) { ?><div > <div id="googleBD"><a href="<?php echo $google_profile; ?>" target="_blank"><span id="google"><?php echo $google_profile; ?></span></a></div><span>گوگــل <img src="g.png"  height="20em" width="20em"  /> </span> </div><?php } ?>
-            <?php if (!empty($instagram_profile)) { ?><div > <div id="instaBD"><a href="<?php echo $instagram_profile; ?>" target="_blank"><span id="insta"><?php echo $instagram_profile; ?></span></a></div> <span>اینستاگـرام <img src="ig.png"  height="20em" width="20em"/> </span> </div><?php } ?>
-            <?php if (!empty($linkin_profile)) { ?><div > <div id="twitterBD"><a href="<?php echo $linkin_profile; ?>" target="_blank"><span id="twitter"><?php echo $linkin_profile; ?></span></a></div> <span>لینکـدین <img src="in.png"  height="20em" width="20em" /> </span> </div><?php } ?>
-            
+                                         
         </div>     <!-- end of right -->
      </div>   <!-- end of detailsPop -->     
    
